03729a9c6940cb30e4f2cb70e323e6e3a62d36df
Benson.YBS <yehbo0921@gmail.com>
2022-10-02 18:02:12 +0800

    Add index page

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
+const restaurantList = require('./restaurant.json')
 //Set view engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 //Set static file
 app.use(express.static('public'))
 
-
+//Routes
+app.get('/', (req, res) => {
+  const restaurants = restaurantList.results
+  res.render('index', { restaurants: restaurants })
+})
 
 
 
