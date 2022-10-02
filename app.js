aaa5039d1a42efb2d132cb3a1230408a709e8b7c
Benson.YBS <yehbo0921@gmail.com>
2022-10-02 17:48:08 +0800

    Install express-handlebars 3.0.0

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
+//Set view engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 
 
