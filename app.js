7e125f3038dcc0c1f58ef5c56d02e51e4cdd94d9
Benson.YBS <yehbo0921@gmail.com>
2022-10-12 23:03:43 +0800

    feat: install method-override 3.0.0, add deletion and detail of restaurant

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const app = express()
 const PORT = 3000
 const Restaurant = require('./models/restaurant.js') //Import restaurant model
@@ -13,12 +14,15 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongo connected!')
 })
+//Set method override
+app.use(methodOverride('_method'))
 //Set view engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 //Set static file
 app.use(express.static('public'))
 //Routes
+//Get all restaurants
 app.get('/', (req, res) => {
   return Restaurant.find()
     .lean()
@@ -26,6 +30,7 @@ app.get('/', (req, res) => {
     .then(restaurants => { res.render('index', { restaurants }) })
     .catch(error => console.log(error))
 })
+//Show restaurant detail
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const id = req.params.restaurant_id
   Restaurant.findById(id)
@@ -34,10 +39,21 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
     .catch(error => console.log(error))
 
 })
+//Delete restaurant
+app.delete('/restaurants/:restaurant_id', (req, res) => {
+  const id = req.params.restaurant_id
+  Restaurant.findById(id)
+    .then(restaurant => { restaurant.remove() })
+    .then(() => {
+      res.redirect('/')
+    })
+    .catch(error => console.log(error))
+})
+//Search for restaurants
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   //Search in mongodb for restaurant name
-  Restaurant.find({name: {$regex: keyword, $options: 'i'}})
+  Restaurant.find({ name: { $regex: keyword, $options: 'i' } })
     .lean()
     .then(restaurants => res.render('index', { restaurants, keyword }))
     .catch(error => console.log(error))
