e001a0899548048997689325cf0c3cfa065c5e9c
Benson.YBS <yehbo0921@gmail.com>
2022-11-07 00:07:40 +0800

    feat: user register

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user.js')
+
+// GET: Regitster Page
+// POST: Register New User
 router.route('/register')
   .get((req, res, next) => {
     res.render('register')
   })
   .post((req, res, next) => {
-    
+    const { name, email, password, confirmPassword } = req.body
+    //Check user existence
+    User.findOne({ email }).then(user => {
+      if (user) {
+        res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      } else {
+        return User.create({ name, email, password })
+          .then(() => { res.redirect('/') })
+          .catch(err => console.log(err))
+      }
+    }
+    ).catch(err => console.log(err))
   })
+// GET:
+// POST:
 router.route('/login')
   .get((req, res, next) => {
     res.render('login')
@@ -14,9 +36,10 @@ router.route('/login')
   .post((req, res, next) => {
 
   })
+// GET:
 router.route('logout')
   .get((req, res, next) => {
-    
+
   })
 
 module.exports = router
