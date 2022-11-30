1c7afbda23014186062be7a770d5eedba74730f4
Benson.YBS <yehbo0921@gmail.com>
2022-11-30 19:18:09 +0800

    feat: add dotenv package Sensitive info can be saved in .env file as environment variable now

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const usePassport = require('./config/passport.js')
 const flash = require('connect-flash')
 const routes = require('./routes')
 const app = express()
-const PORT = 3000
 
+// Set environment variable
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 // Set mongoose
 require('./config/mongoose.js')
 // Set method override
@@ -21,7 +24,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 // Set session
 app.use(session({
-  secret: 'RestaurantSecret',
+  secret: process.env.SESSION_SECRET,
   resave: false, //強制將session更新到session store內，參考 https://www.npmjs.com/package/express-session#resave
   saveUninitialized: true //強制將未初始化的session存回store內，參考 https://www.npmjs.com/package/express-session#saveuninitialized
 }))
@@ -29,6 +32,7 @@ app.use(session({
 usePassport(app)
 // Set Flash message
 app.use(flash())
+
 // Set locals middleware
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
@@ -40,6 +44,6 @@ app.use((req, res, next) => {
 // Set routes
 app.use(routes)
 
-app.listen(PORT, () => {
-  console.log(`Express is running on http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Express is running on http://localhost:${process.env.PORT}`);
 })
